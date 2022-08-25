4a322750beaa9634f42d266b55c69971da025b62
Ana Cristina de Magalhães Araújo (Kika) <105136454+kikamagalhaes@users.noreply.github.com>
2022-08-25 01:49:05 -0300

    adicionando ConsoleApp1 e ConsoleAppCarro

---
+++ ConsoleAppCarro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppCarro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var carro = new Carro();
+            carro.Liga();
+            carro.Acelera(30);
+            carro.Acelera(60);
+
+            Console.WriteLine(carro.ligado);
+            Console.WriteLine(carro.velocidade);
+
+            var carro2 = new Carro();
+            carro2.Liga();
+         
+            carro2.Acelera(110);
+
+
+            Console.WriteLine(carro2.velocidade);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
