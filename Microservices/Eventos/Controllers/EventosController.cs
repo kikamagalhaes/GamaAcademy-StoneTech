259b29cd61b6c88b659ff1de9903ae32784138e0
Ana Cristina de Magalhães Araújo (Kika) <105136454+kikamagalhaes@users.noreply.github.com>
2022-09-03 13:40:18 -0300

    primeira api

---
+++ Microservices/Eventos/Controllers/EventosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Eventos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventosController : ControllerBase
+    {
+        // GET: api/<EventoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EventoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EventoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EventoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
